233d3cf92cb3d7d7b4b8ce039dbe056ea631b41f
Dmitriy <dmitriy250300@mail.ru>
2023-04-08 20:37:45 +0300

    08.04.2023

--- Module 5 PyGame/ppong/pp_game.py
+++ Module 5 PyGame/ppong/pp_game.py
@@ -1,6 +1,5 @@
 from pygame import *
 
-
 class GameSprite(sprite.Sprite):
     '''класс-родитель для других классов'''
     def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
@@ -59,3 +58,45 @@ lose1 = font.render('ИГРОК 1 ПРОИГРАЛ', True, (149, 0, 0))
 lose2 = font.render('ИГРОК 2 ПРОИГРАЛ', True, (149, 0, 0))
 
 #флаги, отвечающие за состояние игры
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if not finish:
+        window.fill(back)
+        racket1.move_left()
+        racket2.move_right()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= -1
+
+        #если мяч достигает границ экрана, меняем направление его движения
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
